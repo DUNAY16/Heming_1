ec3afcb688a4029eef70f40f907541efa5c99abf
валера кравец <kravetz.valera2015@gmail.com>
2021-01-15 16:41:50 +0200

    Добавьте файлы проекта.

---
+++ Heming_1/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Heming_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(310, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(584, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Виконати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(584, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(59, 291);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 147);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Дія";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.radioButton1.Location = new System.Drawing.Point(6, 48);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(154, 36);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Кодувати";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.radioButton2.Location = new System.Drawing.Point(6, 98);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(182, 36);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Декодувати";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(407, 332);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(337, 22);
+            this.textBox3.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold);
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(30, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 32);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Довжина інф. слова";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.comboBox1.Location = new System.Drawing.Point(36, 219);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(64, 24);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold);
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(24, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 32);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Вихідний файл";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold);
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 32);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Вхідний файл";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(401, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 32);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Результат";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Heming_1.Properties.Resources.zamok_lock_closed;
+            this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 332);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Heming_1.Properties.Resources.kluch;
+            this.pictureBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 388);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 42);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 25;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Heming_1.Properties.Resources._3tochku;
+            this.button3.Image = global::Heming_1.Properties.Resources._3tochku;
+            this.button3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button3.Location = new System.Drawing.Point(346, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 22);
+            this.button3.TabIndex = 16;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::Heming_1.Properties.Resources._3tochku;
+            this.button4.Image = global::Heming_1.Properties.Resources._3tochku;
+            this.button4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button4.Location = new System.Drawing.Point(346, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 22);
+            this.button4.TabIndex = 15;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(202)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Heming_1/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Heming_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+            static BitArray ConvertFileToBitArray(string path)
+            {
+                byte[] fileBytes = File.ReadAllBytes(path);
+                BitArray messageArray = new BitArray(fileBytes);
+
+                return messageArray;
+            }
+
+        //static void WriteBitArrayToFile(string fileName, BitArray bitArray)
+        //{
+        //    byte[] bytes = new byte[bitArray.Length / 8 + (bitArray.Length % 8 == 0 ? 0 : 1)];
+        //    bitArray.CopyTo(bytes, 0);
+
+        //    string path = Directory.GetCurrentDirectory() + "\\" + fileName;
+
+        //    //File.Create(path);
+        //    using (FileStream fs = File.Create(path))
+        //    {
+        //        fs.Write(bytes, 0, bytes.Length);
+        //    }
+        //}
+        public static byte[] BitArrayToBytes(System.Collections.BitArray messageCoded1) // переводимо масив бітів в масив байтів
+        {
+            if (messageCoded1.Length == 0)
+            {
+                throw new System.ArgumentException("must have at least length 1", "bitarray");
+            }
+
+            int num_bytes = messageCoded1.Length / 8;
+
+            if (messageCoded1.Length % 8 != 0)
+            {
+                num_bytes += 1;
+            }
+
+            var bytes = new byte[num_bytes];
+            messageCoded1.CopyTo(bytes, 0);
+            return bytes;
+        }
+
+        static BitArray MyCoding(BitArray messageArray)
+            {
+                int countBits = messageArray.Count; // кількість біт в масиві
+                BitArray messageCoded = new BitArray(countBits, false); // новий пустий масив біт
+
+                //for (int i = 0; i < countBits; i+=2) // якесь кодування (це заміни на те що потрібно)
+                //{
+                //    messageCoded[i] = messageArray[i] ^ true;
+                //    messageCoded[i+1] = messageArray[i+1] ^ false;
+                //}
+                for (int i = 0; i < countBits; i++)
+                {
+                    if (messageArray[i] == true)
+                        messageCoded[i] = true;
+                    else
+                        messageCoded[i] = false;
+                }
+                int messageInd = 0;
+                int retInd = 0;
+                int controlIndex = 1;
+                var retArray = new BitArray(messageCoded.Length + 1 + (int)Math.Ceiling(Math.Log(messageCoded.Length, 2)));
+                while (messageInd < messageCoded.Length)
+                {
+                    if (retInd + 1 == controlIndex)
+                    {
+                        retInd++;
+                        controlIndex = controlIndex * 2;
+                        continue;
+                    }
+                    retArray.Set(retInd, messageCoded.Get(messageInd));
+                    messageInd++;
+                    retInd++;
+                }
+                retInd = 0;
+                controlIndex = 1 << (int)Math.Log(retArray.Length, 2);
+                while (controlIndex > 0)
+                {
+                    int c = controlIndex - 1;
+                    int counter = 0;
+
+                    while (c < retArray.Length)
+                    {
+                        for (int i = 0; i < controlIndex && c < retArray.Length; i++)
+                        {
+                            if (retArray.Get(c))
+                                counter++;
+                            c++;
+                        }
+                        c += controlIndex;
+                    }
+
+                    if (counter % 2 != 0) retArray.Set(controlIndex - 1, true);
+                    controlIndex = controlIndex / 2;
+                }
+                return retArray;
+                //return messageCoded;
+            }
+
+            static BitArray MyDeCoding(BitArray messageArray)
+            {
+                int countBits = messageArray.Count; // кількість біт в масиві
+                BitArray messageCoded = new BitArray(countBits, false); // новий пустий масив біт
+
+                for (int i = 0; i < countBits; i++)
+                {
+                    if (messageArray[i] == true)
+                        messageCoded[i] = true;
+                    else
+                        messageCoded[i] = false;
+                }
+                var decodedArray = new BitArray((int)(messageCoded.Count - Math.Ceiling(Math.Log(messageCoded.Count, 2))), false);
+                int count = 0;
+                for (int i = 0; i < messageCoded.Length; i++)
+                {
+                    for (int j = 0; j < Math.Ceiling(Math.Log(messageCoded.Count, 2)); j++)
+                    {
+                        if (i == Math.Pow(2, j) - 1)
+                            i++;
+                    }
+                    decodedArray[count] = messageCoded[i];
+                    count++;
+                }
+                BitArray strDecodedArray = new BitArray(countBits);
+                for (int i = 0; i < decodedArray.Length; i++)
+                {
+                    if (decodedArray[i])
+                        strDecodedArray[i] = true;
+                    else
+                        strDecodedArray[i] = false;
+                }
+                var checkArray = MyCoding(strDecodedArray);
+                byte[] failBits = new byte[checkArray.Length - decodedArray.Length];
+                count = 0;
+                bool isMistake = false;
+                for (int i = 0; i < checkArray.Length - decodedArray.Length; i++)
+                {
+                    if (messageCoded[(int)Math.Pow(2, i) - 1] != checkArray[(int)Math.Pow(2, i) - 1])
+                    {
+                        failBits[count] = (byte)(Math.Pow(2, i));
+                        count++;
+                        isMistake = true;
+                    }
+                }
+                if (isMistake)
+                {
+                    int mistakeIndex = 0;
+                    for (int i = 0; i < failBits.Length; i++)
+                        mistakeIndex += failBits[i];
+                    mistakeIndex--;
+                    messageCoded.Set(mistakeIndex, !messageCoded[mistakeIndex]);
+                    Console.WriteLine($"Ошибка в бите №{mistakeIndex}");
+                    count = 0;
+                    for (int i = 0; i < messageCoded.Length; i++)
+                    {
+                        for (int j = 0; j < Math.Ceiling(Math.Log(messageCoded.Count, 2)); j++)
+                        {
+                            if (i == Math.Pow(2, j) - 1)
+                                i++;
+                        }
+                        decodedArray[count] = messageCoded[i];
+                        count++;
+                    }
+                }
+                return decodedArray;
+            }
+
+            bool flag = false;
+            bool flag1 = false;
+        string abc;
+        string abcd;
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                if (flag == true)
+                {
+                    //File.Create(Directory.GetCurrentDirectory() + "\\test1.txt");
+                    //string path1 = Directory.GetCurrentDirectory() + "\\test1.txt";
+                    if (File.Exists(abc) == false) return; // перевірка на наявність файла
+                    BitArray messageArray = ConvertFileToBitArray(abc); // читаємо файл і записуємо у BitArray
+                    BitArray messageCoded = MyCoding(messageArray); // кодуємо bitArray
+
+                //BitArrayToBytes(abcd, messageCoded); // записуємо bitArray у файл
+                System.IO.File.WriteAllBytes(abcd, BitArrayToBytes(messageCoded));
+                MessageBox.Show("Файл Сохранен");
+            }
+                if (flag1 == true)
+                {
+                    //string path1 = Directory.GetCurrentDirectory() + "\\test2.txt";
+                    if (File.Exists(abc) == false) return;
+                    BitArray messageArray = ConvertFileToBitArray(abc);
+                    BitArray messageCoded = MyDeCoding(messageArray);
+
+                //WriteBitArrayToFile(abcd, messageCoded);
+                System.IO.File.WriteAllBytes(abcd, BitArrayToBytes(messageCoded));
+            }
+
+                flag = false;
+                flag1 = false;
+
+
+            }
+
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void button4_Click(object sender, EventArgs e)
+            {
+
+                if (flag == true || flag1 == true)
+                {
+                if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                string filename = openFileDialog1.FileName;
+                var fileText = System.IO.File.ReadAllBytes(filename);
+                textBox1.Text = filename;
+                abc = filename;
+            }
+        }
+
+            private void button3_Click(object sender, EventArgs e)
+            {
+            if (flag == true || flag1 == true)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                string filename = saveFileDialog1.FileName;
+                textBox2.Text = filename;
+                abcd = filename;
+            }
+        }
+
+            private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+            {
+                flag = true;
+            }
+
+            private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+            {
+                flag1 = true;
+            }
+
+            private void button2_Click_1(object sender, EventArgs e)
+            {
+            
+            flag = false;
+            flag1 = false;
+            }
+        }
+    }
+
